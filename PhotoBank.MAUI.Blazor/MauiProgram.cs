81eaa67584db4ece4a811c337eda66c4cfa61de5
Alexandr Makhin <amakhin@gmail.com>
2024-03-03 16:11:05 +0100

    add radzen

--- PhotoBank.MAUI.Blazor/MauiProgram.cs
+++ PhotoBank.MAUI.Blazor/MauiProgram.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.Extensions.Logging;
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using Radzen;
+using System.Reflection;
 
 namespace PhotoBank.MAUI.Blazor
 {
@@ -14,10 +17,26 @@ namespace PhotoBank.MAUI.Blazor
                     fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
                 });
 
+            // Load appsettings.json
+            using var appsettingsStream = Assembly
+                .GetExecutingAssembly()
+                .GetManifestResourceStream("MyApp.wwwroot.appsettings.json");
+
+            if (appsettingsStream != null)
+            {
+                var config = new ConfigurationBuilder()
+                    .AddJsonStream(appsettingsStream)
+                    .Build();
+
+                builder.Configuration.AddConfiguration(config);
+            }
+
             builder.Services.AddMauiBlazorWebView();
 
+            builder.Services.AddRadzenComponents();
+
 #if DEBUG
-    		builder.Services.AddBlazorWebViewDeveloperTools();
+            builder.Services.AddBlazorWebViewDeveloperTools();
     		builder.Logging.AddDebug();
 #endif
 
