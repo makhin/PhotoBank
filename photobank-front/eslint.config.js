c883981de297b496ffaf024981240e39705d9aeb
Alexandr Makhin <amakhin@gmail.com>
2025-06-20 13:38:01 +0200

    eslint

--- photobank-front/eslint.config.js
+++ photobank-front/eslint.config.js
@@ -1,28 +1,65 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
+import js from '@eslint/js';
+import globals from 'globals';
+import tseslint from '@typescript-eslint/eslint-plugin';
+import parser from '@typescript-eslint/parser';
+import react from 'eslint-plugin-react';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import jsxA11y from 'eslint-plugin-jsx-a11y';
+import importPlugin from 'eslint-plugin-import';
 
-export default tseslint.config(
-  { ignores: ['dist'] },
-  {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
-    files: ['**/*.{ts,tsx}'],
-    languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
+/** @type {import("eslint").Linter.FlatConfig[]} */
+export default [
+    js.configs.recommended,
+    {
+        files: ['**/*.{ts,tsx}'],
+        ignores: ['dist', 'build'],
+        languageOptions: {
+            parser,
+            parserOptions: {
+                project: './tsconfig.json',
+                sourceType: 'module',
+                ecmaVersion: 'latest',
+                ecmaFeatures: {jsx: true, tsx: true},
+            },
+            globals: globals.browser,
+        },
+        plugins: {
+            '@typescript-eslint': tseslint,
+            react,
+            'react-hooks': reactHooks,
+            'react-refresh': reactRefresh,
+            'jsx-a11y': jsxA11y,
+            import: importPlugin,
+        },
+        rules: {
+            // TypeScript
+            '@typescript-eslint/no-explicit-any': 'warn',
+            '@typescript-eslint/consistent-type-imports': 'warn',
+
+            // React
+            'react/react-in-jsx-scope': 'off', // React 17+
+            'react/prop-types': 'off',
+
+            // React Hooks
+            'react-hooks/rules-of-hooks': 'error',
+            'react-hooks/exhaustive-deps': 'warn',
+
+            // JSX Accessibility
+            'jsx-a11y/alt-text': 'warn',
+
+            // Import sorting
+            'import/order': [
+                'warn',
+                {
+                    groups: [['builtin', 'external'], ['internal'], ['parent', 'sibling', 'index']],
+                    'newlines-between': 'always',
+                    alphabetize: {order: 'asc', caseInsensitive: true},
+                },
+            ],
+
+            // React Fast Refresh
+            'react-refresh/only-export-components': ['warn', {allowConstantExport: true}],
+        },
     },
-    plugins: {
-      'react-hooks': reactHooks,
-      'react-refresh': reactRefresh,
-    },
-    rules: {
-      ...reactHooks.configs.recommended.rules,
-      'react-refresh/only-export-components': [
-        'warn',
-        { allowConstantExport: true },
-      ],
-    },
-  },
-)
+];
