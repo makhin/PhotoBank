7baebf023bc2e5bb77576c42671861635b2d2c97
Alexandr Makhin <amakhin@gmail.com>
2025-06-21 21:33:48 +0200

    eslint

--- photobank-front/eslint.config.mjs
+++ photobank-front/eslint.config.mjs
@@ -1,9 +1,37 @@
 // @ts-check
 
-import eslint from '@eslint/js';
-import tseslint from 'typescript-eslint';
+import eslint from "@eslint/js";
+import tseslint from "typescript-eslint";
+import reactPlugin from "eslint-plugin-react";
+import reactHooks from "eslint-plugin-react-hooks";
 
 export default tseslint.config(
-    eslint.configs.recommended,
-    tseslint.configs.recommended,
+  ...tseslint.configs.strictTypeChecked, // or recommendedTypeChecked for less strictness
+  {
+    plugins: {
+      react: reactPlugin,
+      "react-hooks": reactHooks,
+    },
+    languageOptions: {
+      parserOptions: {
+        project: ["./tsconfig.node.json", "./tsconfig.app.json"],
+        tsconfigRootDir: import.meta.dirname,
+        ecmaFeatures: { jsx: true },
+      },
+    },
+    settings: {
+      react: {
+        version: "detect",
+      },
+    },
+    rules: {
+      // React best practices
+      ...reactPlugin.configs.recommended.rules,
+      ...reactPlugin.configs["jsx-runtime"].rules,
+      ...reactHooks.configs.recommended.rules,
+      // Add or override rules as needed
+      "react/react-in-jsx-scope": "off", // Not needed for React 17+
+      "react/prop-types": "off", // Not needed with TypeScript
+    },
+  }
 );
--- photobank-front/src/entities/photo/api.ts
+++ photobank-front/src/entities/photo/api.ts
@@ -1,24 +1,25 @@
-import {createApi, fetchBaseQuery} from '@reduxjs/toolkit/query/react'
-import type {FilterDto, PhotoDto, QueryResult,} from '@/entities/meta/model.ts'
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import type {
+  FilterDto,
+  PhotoDto,
+  QueryResult,
+} from "@/entities/photo/model.ts";
 
 export const api = createApi({
-    reducerPath: 'photobankApi',
-    baseQuery: fetchBaseQuery({baseUrl: '/api'}),
-    endpoints: (builder) => ({
-        getPhotoById: builder.query<PhotoDto, number>({
-            query: (id) => `photos/${id}`,
-        }),
-        searchPhotos: builder.mutation<QueryResult, FilterDto>({
-            query: (filter) => ({
-                url: 'photos/search',
-                method: 'POST',
-                body: filter,
-            }),
-        }),
+  reducerPath: "photobankApi",
+  baseQuery: fetchBaseQuery({ baseUrl: "/api" }),
+  endpoints: (builder) => ({
+    getPhotoById: builder.query<PhotoDto, number>({
+      query: (id) => `photos/${id.toString()}`,
     }),
-})
+    searchPhotos: builder.mutation<QueryResult, FilterDto>({
+      query: (filter) => ({
+        url: "photos/search",
+        method: "POST",
+        body: filter,
+      }),
+    }),
+  }),
+});
 
-export const {
-    useGetPhotoByIdQuery,
-    useSearchPhotosMutation,
-} = api
+export const { useGetPhotoByIdQuery, useSearchPhotosMutation } = api;
