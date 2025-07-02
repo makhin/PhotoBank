e114f3d225836b8a5d0f1b61e2b05ea90a2c0884
Alexandr Makhin <amakhin@gmail.com>
2025-07-02 20:08:10 +0200

    Add Swagger JWT security

--- PhotoBank.Api/Program.cs
+++ PhotoBank.Api/Program.cs
@@ -7,6 +7,7 @@ using PhotoBank.Services;
 using PhotoBank.Api.Middleware;
 using Serilog.Events;
 using Serilog;
+using Microsoft.OpenApi.Models;
 
 namespace PhotoBank.Api
 {
@@ -93,7 +94,33 @@ namespace PhotoBank.Api
             builder.Services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddSwaggerGen();
+            builder.Services.AddSwaggerGen(options =>
+            {
+                options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                {
+                    Name = "Authorization",
+                    Type = SecuritySchemeType.Http,
+                    Scheme = "bearer",
+                    BearerFormat = "JWT",
+                    In = ParameterLocation.Header,
+                    Description = "Enter JWT token"
+                });
+
+                options.AddSecurityRequirement(new OpenApiSecurityRequirement
+                {
+                    {
+                        new OpenApiSecurityScheme
+                        {
+                            Reference = new OpenApiReference
+                            {
+                                Type = ReferenceType.SecurityScheme,
+                                Id = "Bearer"
+                            }
+                        },
+                        Array.Empty<string>()
+                    }
+                });
+            });
 
             RegisterServicesForApi.Configure(builder.Services);
             builder.Services.AddAutoMapper(typeof(MappingProfile));
