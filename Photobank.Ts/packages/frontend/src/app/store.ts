fc31aafec96e639909ab0459ee9af724595e1d5d
Alexandr Makhin <amakhin@gmail.com>
2025-07-03 19:22:22 +0200

    feat(frontend): persist filter and list photos

--- Photobank.Ts/packages/frontend/src/app/store.ts
+++ Photobank.Ts/packages/frontend/src/app/store.ts
@@ -1,11 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit';
 
 import metaReducer from '@/features/meta/model/metaSlice.ts';
+import photoReducer from '@/features/photo/model/photoSlice.ts';
 import { api } from '@/entities/photo/api.ts';
 
 export const store = configureStore({
   reducer: {
     metadata: metaReducer,
+    photo: photoReducer,
     [api.reducerPath]: api.reducer,
   },
   middleware: (getDefaultMiddleware) =>
--- Photobank.Ts/packages/frontend/src/pages/filter/FilterPage.tsx
+++ Photobank.Ts/packages/frontend/src/pages/filter/FilterPage.tsx
@@ -2,16 +2,22 @@ import {useForm} from 'react-hook-form';
 import {zodResolver} from '@hookform/resolvers/zod';
 import type {z} from 'zod';
 
-import {Button} from '@/components/ui/button';
-import {Card, CardContent, CardHeader, CardTitle} from '@/components/ui/card';
-import {Form} from '@/components/ui/form';
-import {formSchema} from '@/features/filter/lib/form-schema.ts';
-import {FilterFormFields} from '@/components/FilterFormFields.tsx';
+import { useNavigate } from 'react-router-dom';
+import { useAppDispatch } from '@/app/hook.ts';
+import { setFilter } from '@/features/photo/model/photoSlice.ts';
+import { Button } from '@/components/ui/button';
+import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
+import { Form } from '@/components/ui/form';
+import { formSchema } from '@/features/filter/lib/form-schema.ts';
+import { FilterFormFields } from '@/components/FilterFormFields.tsx';
 
 // Infer FormData type from formSchema to ensure compatibility
 type FormData = z.infer<typeof formSchema>;
 
 function FilterPage() {
+  const dispatch = useAppDispatch();
+  const navigate = useNavigate();
+
   const form = useForm<FormData>({
     resolver: zodResolver(formSchema),
     defaultValues: {
@@ -30,7 +36,22 @@ function FilterPage() {
   });
 
   const onSubmit = (data: FormData) => {
-    console.log('Form submitted:', data);
+    const filter = {
+      caption: data.caption,
+      storages: data.storages?.map(Number),
+      paths: data.paths?.map(Number),
+      persons: data.persons?.map(Number),
+      tags: data.tags?.map(Number),
+      isBW: data.isBW,
+      isAdultContent: data.isAdultContent,
+      isRacyContent: data.isRacyContent,
+      thisDay: data.thisDay,
+      takenDateFrom: data.dateFrom?.toISOString(),
+      takenDateTo: data.dateTo?.toISOString(),
+    } as const;
+
+    dispatch(setFilter(filter));
+    navigate('/photos');
   };
 
   return (
