1cfdcd0a925e7d6082f19fdb2d4e8a0c5bf7efd0
Alexandr Makhin <amakhin@gmail.com>
2025-07-04 08:54:46 +0200

    small fixes

--- PhotoBank.Services/Api/TokenService.cs
+++ PhotoBank.Services/Api/TokenService.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
 using System.Text;
@@ -15,12 +16,10 @@ public interface ITokenService
 
 public class TokenService(IConfiguration configuration) : ITokenService
 {
-    private readonly IConfiguration _configuration = configuration;
-
     public string CreateToken(ApplicationUser user)
     {
-        var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["Jwt:Key"]!));
-        var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+        var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration["Jwt:Key"]!));
+        var credentials = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
 
         var claims = new List<Claim>
         {
@@ -31,11 +30,11 @@ public class TokenService(IConfiguration configuration) : ITokenService
         };
 
         var token = new JwtSecurityToken(
-            issuer: _configuration["Jwt:Issuer"],
-            audience: _configuration["Jwt:Audience"],
+            issuer: configuration["Jwt:Issuer"],
+            audience: configuration["Jwt:Audience"],
             claims: claims,
             expires: DateTime.UtcNow.AddHours(1),
-            signingCredentials: creds);
+            signingCredentials: credentials);
 
         return new JwtSecurityTokenHandler().WriteToken(token);
     }
