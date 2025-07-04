559497300346d62835510173156784e6136b25a9
Alexandr Makhin <amakhin@gmail.com>
2025-07-04 16:00:47 +0200

    fix

--- Photobank.Ts/packages/shared/src/config.ts
+++ Photobank.Ts/packages/shared/src/config.ts
@@ -1,6 +1,14 @@
-export const isBrowser = typeof window !== 'undefined';
+export function isBrowser(): boolean {
+    return typeof window !== 'undefined' && typeof window.crypto !== 'undefined';
+}
 
-export const API_BASE_URL = isBrowser
+export function isNode(): boolean {
+    return typeof process !== 'undefined' &&
+        process.versions != null &&
+        process.versions.node != null;
+}
+
+export const API_BASE_URL = isBrowser()
     ? (import.meta as any).env?.VITE_API_BASE_URL
     // @ts-ignore
     : (typeof process !== 'undefined' ? process.env.API_BASE_URL : undefined) ?? 'http://192.168.1.45:5066';
