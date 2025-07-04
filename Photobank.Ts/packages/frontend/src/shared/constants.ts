5062cacb721667f554d66930489c6eec30eacaf2
Alexandr Makhin <amakhin@gmail.com>
2025-07-04 18:24:13 +0200

    chore: unify frontend API env variable

--- Photobank.Ts/packages/frontend/src/shared/constants.ts
+++ Photobank.Ts/packages/frontend/src/shared/constants.ts
@@ -1,4 +1,4 @@
-export const BASE_URL: string = import.meta.env.VITE_API_URL as string;
+export const BASE_URL: string = import.meta.env.API_BASE_URL as string;
 export const METADATA_CACHE_KEY = 'photobank_metadata_cache';
 export const METADATA_CACHE_VERSION = 1;
 
