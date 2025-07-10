6776609991906a496f45027e7675f051c44ce356
Alexandr Makhin <amakhin@gmail.com>
2025-07-10 17:22:35 +0200

    fix: avoid double requests during search

--- Photobank.Ts/packages/frontend/src/main.tsx
+++ Photobank.Ts/packages/frontend/src/main.tsx
@@ -11,11 +11,9 @@ import './index.css';
 
 // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </Provider>
-  </React.StrictMode>
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>
 );
