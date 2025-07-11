755b1ae54cdf6f6ad338326b24174cafd4d5782d
Alexandr Makhin <amakhin@gmail.com>
2025-07-11 20:50:31 +0200

    Extend token generation with user and role claims

---
+++ PhotoBank.UnitTests/TokenServiceTests.cs
@@ -0,0 +1,42 @@
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Linq;
+using Microsoft.Extensions.Configuration;
+using NUnit.Framework;
+using PhotoBank.DbContext.Models;
+using PhotoBank.Services.Api;
+
+namespace PhotoBank.UnitTests;
+
+[TestFixture]
+public class TokenServiceTests
+{
+    [Test]
+    public void CreateToken_WithAdditionalClaims_ShouldContainThem()
+    {
+        var configuration = new ConfigurationBuilder()
+            .AddInMemoryCollection(new Dictionary<string, string?>
+            {
+                ["Jwt:Key"] = "VerySecretKeyVerySecretKeyVerySecretKey",
+                ["Jwt:Issuer"] = "test",
+                ["Jwt:Audience"] = "test"
+            })
+            .Build();
+
+        var service = new TokenService(configuration);
+        var user = new ApplicationUser
+        {
+            Id = "1",
+            Email = "user@example.com",
+            UserName = "user"
+        };
+
+        var claims = new[] { new Claim("AllowAdultContent", "True") };
+
+        var token = service.CreateToken(user, false, claims);
+        var jwt = new JwtSecurityTokenHandler().ReadJwtToken(token);
+
+        Assert.That(jwt.Claims.Any(c => c.Type == "AllowAdultContent" && c.Value == "True"));
+    }
+}
