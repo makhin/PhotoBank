faf523e400f108b61627843bb6ff4a64c6a98470
Alexandr Makhin <amakhin@gmail.com>
2025-08-15 15:14:56 +0300

    Update orval.config.ts

--- orval.config.ts
+++ orval.config.ts
@@ -5,6 +5,7 @@ export default defineConfig({
     output: {
       target: 'packages/shared/src/api-frontend.ts',
       client: 'react-query',
+ mode: 'tags-split',
     },
   },
   bot: {
@@ -12,6 +13,7 @@ export default defineConfig({
     output: {
       target: 'packages/telegram-bot/src/api/generated.ts',
       client: 'axios',
+ mode: 'tags-split',
       override: { mutator: { path: './axios-instance.ts', name: 'photobankAxios' } },
     },
   },
