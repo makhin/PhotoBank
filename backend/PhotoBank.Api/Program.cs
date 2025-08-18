2bb83dfc73488a92e0bec6de903b6bb4ae930b21
Alexandr Makhin <amakhin@gmail.com>
2025-08-18 19:54:49 +0300

    chore: use shared access control registration

--- backend/PhotoBank.Api/Program.cs
+++ backend/PhotoBank.Api/Program.cs
@@ -177,7 +177,7 @@ namespace PhotoBank.Api
             });
 
             builder.Services.AddScoped<IEffectiveAccessProvider, EffectiveAccessProvider>();
-            builder.Services.AddScoped<ICurrentUser, CurrentUser>();
+            builder.Services.AddScoped<PhotoBank.AccessControl.ICurrentUser, PhotoBank.AccessControl.CurrentUser>();
 
             var app = builder.Build();
 
