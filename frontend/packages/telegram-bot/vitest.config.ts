8ea2c024d023fbbd6e0fffa4a57a7f74b2718a1f
Alexandr Makhin <amakhin@gmail.com>
2025-08-19 19:28:45 +0300

    test(telegram-bot): setup MSW server for tests

--- frontend/packages/telegram-bot/vitest.config.ts
+++ frontend/packages/telegram-bot/vitest.config.ts
@@ -1,3 +1,14 @@
+import { defineConfig, mergeConfig } from 'vitest/config';
+import baseConfig from '../vitest.base';
+
 process.env.BOT_TOKEN ??= 'test-token';
 process.env.API_BASE_URL ??= 'http://localhost';
-export { default } from '../vitest.base';
+
+export default mergeConfig(
+  baseConfig,
+  defineConfig({
+    test: {
+      setupFiles: './test-setup.ts',
+    },
+  }),
+);
