6e826440f49332a0d8d6eb6cabde3f365f729302
Alexandr Makhin <amakhin@gmail.com>
2025-08-22 19:02:49 +0300

    fix: ignore tags and persons during serialization

--- backend/PhotoBank.ViewModel.Dto/FilterDto.cs
+++ backend/PhotoBank.ViewModel.Dto/FilterDto.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text.Json.Serialization;
 
 namespace PhotoBank.ViewModel.Dto
 {
@@ -16,8 +17,12 @@ namespace PhotoBank.ViewModel.Dto
         public DateTime? TakenDateFrom { get; set; }
         public DateTime? TakenDateTo { get; set; }
         public ThisDayDto? ThisDay { get; set; }
+        [JsonIgnore]
         public IEnumerable<int>? Persons { get; set; }
+        public string[]? PersonNames { get; set; }
+        [JsonIgnore]
         public IEnumerable<int>? Tags { get; set; }
+        public string[]? TagNames { get; set; }
 
         public string? OrderBy { get; set; }
 
@@ -25,7 +30,9 @@ namespace PhotoBank.ViewModel.Dto
         {
             return (Storages != null && Storages.Any())
                    || (Persons != null && Persons.Any())
+                   || (PersonNames != null && PersonNames.Any())
                    || (Tags != null && Tags.Any())
+                   || (TagNames != null && TagNames.Any())
                    || (Paths != null && Paths.Any())
                    || !string.IsNullOrEmpty(RelativePath)
                    || IsBW.HasValue
