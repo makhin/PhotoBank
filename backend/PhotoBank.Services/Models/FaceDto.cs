3fb932b80ccebb90a6845431e9f08a90ff32bcb5
Alexandr Makhin <amakhin@gmail.com>
2025-08-31 09:21:24 +0200

    refactor: remove S3 key from face dto

--- backend/PhotoBank.Services/Models/FaceDto.cs
+++ backend/PhotoBank.Services/Models/FaceDto.cs
@@ -10,8 +10,6 @@ namespace PhotoBank.Services.Models
         public int Id { get; set; }
         public int? PersonId { get; set; }
         [Required]
-        public string S3Key_Image { get; set; }
-        [Required]
         public IdentityStatus IdentityStatus { get; set; }
         public DateTime? PersonDateOfBirth { get; set; }
         public DateTime? PhotoTakenDate { get; set; }
