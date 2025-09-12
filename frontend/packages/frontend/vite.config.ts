6e58de2792d856889ad639e2b62f673ac51f6e5a
Alexandr Makhin <amakhin@gmail.com>
2025-09-12 21:49:56 +0200

    fix

--- frontend/packages/frontend/vite.config.ts
+++ frontend/packages/frontend/vite.config.ts
@@ -2,16 +2,19 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tsconfigPaths from 'vite-tsconfig-paths';
 import * as path from 'node:path';
+import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
   plugins: [
     react(),
+    tailwindcss(),
     tsconfigPaths({
       // читаем только фронтовый tsconfig, игнорим ошибки в остальных пакетах
       projects: [path.resolve(__dirname, 'tsconfig.app.json')],
       ignoreConfigErrors: true,
     }),
   ],
+  server: { port: 5173, host: '0.0.0.0' },
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
