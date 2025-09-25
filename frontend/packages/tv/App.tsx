a0d1682bd932b384810fc24538425f6849d9ddf8
Alexandr Makhin <amakhin@gmail.com>
2025-09-25 15:54:35 +0200

    remove tv

--- frontend/packages/tv/App.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-import { AppNavigator } from './utils/navigation';
-import { useAutoLogin } from './hooks/useAutoLogin';
-
-export default function App() {
-  useAutoLogin();
-  return <AppNavigator />;
-}
--- frontend/packages/tv/config.ts
+++
@@ -1,5 +0,0 @@
-export const API_EMAIL: string = 'amakhin@gmail.com';
-export const API_PASSWORD: string = 'Omega0))';
-if (!API_EMAIL || !API_PASSWORD) {
-    throw new Error('API_EMAIL or API_PASSWORD is not defined');
-}
